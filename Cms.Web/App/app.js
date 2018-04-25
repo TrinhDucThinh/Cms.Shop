7a340842050b91c62c646ecf251c0282bcdfe861
ThinhTD <thinhtd2401@gmail.com>
2018-04-25 09:13:56 +0700

    Add admin screen

---
+++ Cms.Web/App/app.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="E:\MyProject\Git\Cms.Shop\Cms.Web\Assets/Admin/Libs/angular/angular.js" />
+
+(function () {
+    angular.module('cmsShop', []);
+    //config.$inject = ['$stateProvider','$urlRouterConifg'];
+})();
